352a6faab73447087d61a1ea77f97be82be90326
Bruno Thiago Leite Agutoli <brunotla1@gmail.com>
2012-05-19 01:40:16 -0300

    added php document

--- Iptc/Exception.php
+++ Iptc/Exception.php
@@ -1,4 +1,27 @@
 <?php
+<?php
+/**
+ * Class for manipulating EXIF and IPTC image
+ *
+ * PHP version 5
+ *
+ * @category Image
+ * @package  Iptc
+ * @author   Bruno Thiago Leite Agutoli <brunotla1@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/agutoli
+ */
+
+/**
+ * Class for manipulating EXIF and IPTC image
+ *
+ * @category Image
+ * @package  Iptc
+ * @author   Bruno Thiago Leite Agutoli <brunotla1@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @version  Release: 0.0.1
+ * @link     https://github.com/agutoli
+ */
 
 class Iptc_Exception extends Exception 
 {}
