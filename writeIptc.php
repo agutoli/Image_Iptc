400fcb30340d17c7733aeef4df9f738054d65509
Bruno Thiago Leite Agutoli <brunotla1@gmail.com>
2013-04-01 12:25:31 -0300

    test with special characters

--- writeIptc.php
+++ writeIptc.php
@@ -4,6 +4,6 @@ require 'Iptc.php';
 
 $iptc = new Iptc('logo_php.jpg');
 $iptc->set(Iptc::KEYWORDS,array(
-    'keyword1','keyword2'
+    'keyword1','This is a test with special characters ö, ä, ü'
 ));
 $iptc->write();
